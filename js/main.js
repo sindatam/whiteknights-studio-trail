dbc9e0b3dd14fd6181a87c91ed962a8836887936
sindatam <lilli.sinda.tam@gmail.com>
2019-05-11 13:03:46 +0100

    Dropdown menu when mouseover for Category, cSS effect when hover over the category list when expanding and added another page for category list.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+
+// Mobile version for the menu to appear once clicking on the burger menu
 $('.icon').on('click', function() {
     $('.nav-links').toggle();
   });
   
+
+  // Desktop version - mouseover for Category list for the sub-menu to appear
+
+  $('.category-list').on('mouseover', () => {
+    $('.sub-categories-menu-desktop').show()
+  })
+  
+  $('.sub-categories-menu-desktop').on('mouseleave', () => {
+    $('.sub-categories-menu-desktop').hide();
+  })
