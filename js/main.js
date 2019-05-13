4a2ba7811e2ca0a0d0bca3ea55c0298aa4e39196
sindatam <lilli.sinda.tam@gmail.com>
2019-05-13 19:05:27 +0100

    Desktop version to flip images by using CSS

--- js/main.js
+++ js/main.js
@@ -1,43 +1,48 @@
-
 // Mobile version for the menu to appear once clicking on the burger menu
-$('.icon').on('click', function() {
-    $('.nav-links').toggle();
-  });
-  
-
-  // Desktop version - mouseover for Category list for the sub-menu to appear
-
-  $('.category-list').on('mouseover', () => {
-    $('.sub-categories-menu-desktop').show()
-  })
-  
-  $('.sub-categories-menu-desktop').on('mouseleave', () => {
-    $('.sub-categories-menu-desktop').hide();
-  })
+$('.icon').on('click', function () {
+  $('.nav-links').toggle();
+});
 
 
-  // Textile page 
+// Desktop version - mouseover for Category / VWhere on the navbar
 
-  $('.artists-list').on('click', function() {
-    $('.artists-list-menu').toggle();
-  })
+$('.category-list').on('mouseover', () => {
+  $('.sub-categories-menu-desktop').show()
+})
+
+$('.sub-categories-menu-desktop').on('mouseleave', () => {
+  $('.sub-categories-menu-desktop').hide();
+})
+
+$('.where-list').on('mouseover', () => {
+  $('.sub-categories-where').show()
+})
+
+$('.sub-categories-where').on('mouseleave', () => {
+  $('.sub-categories-where').hide();
+})
 
 
-  // artist textile page - read more/less for both desktop and mobile
+// Textile page 
+
+$('.artists-list').on('click', function () {
+  $('.artists-list-menu').toggle();
+})
+
+
+// artist textile page - read more/less for both desktop and mobile
 function myFunction() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
   }
 }
-  
-  
